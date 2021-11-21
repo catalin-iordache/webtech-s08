dd924cc534793decaba2cbe9fa0f7bb295edc66c
razboinic096 <razboinic096@yahoo.com>
2021-11-21 20:51:00 +0200

    filter employee

---
+++ routes/employeeWithFilterByFirstName.js
@@ -0,0 +1,61 @@
+const { Op, Sequelize } = require("sequelize");
+const Employee = require("../models/employee");
+
+const router = require("express").Router();
+
+router
+  .route("/employees")
+  .get(async (req, res) => {
+    // get all
+    const { simplified, sortBy, minSalary, name } = req.query;
+    // aici sunt incluse filtrarea suplimentara (exercitiul 5.), proiectia, sortarea
+    let whereClause = {}  
+    if (minSalary)
+      whereClause.salary =  { [Op.gt]: minSalary };
+
+    if (name)
+      whereClause.firstName = name;  
+
+    try {
+      const employees = await Employee.findAll({
+        attributes: simplified ? { exclude: "id" } : undefined,
+        order: sortBy ? [[sortBy, "ASC"]] : undefined,
+        where:  whereClause,
+      });
+      return res.status(200).json(employees);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  })
+  .post(async (req, res) => {
+    // create
+    // console.log("req.body :>> ", req.body);
+    try {
+      const newEmployee = await Employee.create(req.body);
+      return res.status(200).json(newEmployee);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  });
+
+router
+  .route("/employees/:id")
+  .get(async (req, res) => {
+    // get by id
+    // filtrarea dupa un camp
+    const employee = await Employee.findOne({
+      where: { id: req.params.id },
+    });
+    if (employee) {
+      return res.status(200).json(employee);
+    } else {
+      return res
+        .status(404)
+        .json({ error: `Employee with id ${req.params.id} does not exists` });
+    }
+  })
+  .put(async (req, res) => {
+    // update by id
+  });
+
+module.exports = router;
