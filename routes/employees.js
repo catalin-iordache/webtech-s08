aa58c11d631f9f8a6ce445edcb24991527658116
razboinic096 <razboinic096@yahoo.com>
2021-11-22 09:11:40 +0200

    update

--- routes/employees.js
+++ routes/employees.js
@@ -48,7 +48,14 @@ router
     }
   })
   .put(async (req, res) => {
-    // update by id
+    const employee = await Employee.findByPk(req.params.id);
+      if (employee) {
+        return res.status(200).json(await employee.update(req.body));
+    } else {
+      return res
+        .status(404)
+        .json({ error: `Employee with id ${req.params.id} does not exists` });
+    }
   });
 
 module.exports = router;
